53aae569d607b7ec43cf6cfa0442a546181aed5d
Dong-Yi-Xia <dong7777xia@gmail.com>
2020-08-09 09:25:14 -0400

    added notes

--- lib/song.rb
+++ lib/song.rb
@@ -25,23 +25,28 @@ class Song
   end
 
   def self.create_by_name(title)
+    # song = Song.new_by_name(title)
     song = self.new_by_name(title)
     song.save
     song
   end
 
   def self.find_by_name(title)
+    # Song.all.find do |song| 
     self.all.find do |song| 
       song.name == title 
     end
   end
 
   def self.find_or_create_by_name(title)
+    # found = Song.find_by_name(title)
+    # found ? found : Song.create_by_name(title)
     found = self.find_by_name(title)
     found ? found : self.create_by_name(title)
   end
 
   def self.alphabetical
+    # Song.all.sort_by do |song|
     self.all.sort_by do |song|
       song.name
     end
@@ -49,6 +54,7 @@ class Song
 
   def self.new_from_filename(filename)
     splitname = filename.delete_suffix(".mp3").split(" - ")
+    # song = Song.create
     song = self.create
     song.name = splitname[1]
     song.artist_name = splitname[0]
@@ -57,12 +63,14 @@ class Song
   
 
   def self.create_from_filename(filename)
+    # song = Song.new_from_filename(filename)
     song = self.new_from_filename(filename)
     song.save
     song
   end
 
   def self.destroy_all
+    # Song.all.clear
     self.all.clear
   end
 
